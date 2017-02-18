2134fdff58596d194645badfe03453947e112f14
Patrick Hulce <patrick.hulce@gmail.com>
2017-02-18 13:32:52 -0800

    fix: package.json comma

--- test/app.js
+++ test/app.js
@@ -1,8 +1,10 @@
+var fs = require('fs')
 var path = require('path');
 var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
 
 describe('generator-npm-package:app', function () {
+  var tmpDir = ''
   before(function () {
     return helpers.run(path.join(__dirname, '../generators/app'))
       .withPrompts({
@@ -11,6 +13,9 @@ describe('generator-npm-package:app', function () {
         initGitHub: false,
         initSemanticRelease: false,
         includeBrowserPackaging: true,
+      })
+      .inTmpDir(function (dir) {
+        tmpDir = dir;
       })
       .toPromise();
   });
@@ -27,5 +32,9 @@ describe('generator-npm-package:app', function () {
       'lib/index.js',
       'test/bootstrap.js',
     ]);
+  });
+
+  it('creates valid json', function () {
+    JSON.parse(fs.readFileSync(path.join(tmpDir, 'package.json'), 'utf8'))
   });
 });
